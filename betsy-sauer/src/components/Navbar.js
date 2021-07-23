0f5f2fa707e8723205ca5d9da3898039ef93d72d
Thomas Russell <t.p.russell19@gmail.com>
2021-07-23 16:58:02 -0700

    :lipstick: :construction: WIP: attempting to add some styling to navbar depending on current page

--- betsy-sauer/src/components/Navbar.js
+++ betsy-sauer/src/components/Navbar.js
@@ -1,13 +1,19 @@
+import React from "react";
+
 function Navbar(props) {
 
+  let pageIndicator = '';
+
   return (
-    <ul className='navbar'>
-      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'home')} href='/'>HOME</a></li>
-      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'about')} href='/about'>ABOUT ME</a></li>
-      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'gallery')} href='/gallery'>GALLERY</a></li>
-      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'products')} href='/products'>PRODUCTS</a></li>
-      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'appointment')} href='/appointment'>BOOK AN APPOINTMENT</a></li>
-    </ul>
+    <React.Fragment>
+      <ul className='navbar'>
+        <li className={pageIndicator + ' navbar-items'} {...props.currentPage === 'home' ? pageIndicator='active' : null }><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'home')} href='/'>HOME</a></li>
+        <li className='navbar-items' {...props.currentPage}><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'about')} href='/about'>ABOUT ME</a></li>
+        <li className='navbar-items' {...props.currentPage}><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'gallery')} href='/gallery'>GALLERY</a></li>
+        <li className='navbar-items' {...props.currentPage}><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'products')} href='/products'>PRODUCTS</a></li>
+        <li className='navbar-items' {...props.currentPage}><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'appointment')} href='/appointment'>BOOK AN APPOINTMENT</a></li>
+      </ul>
+    </React.Fragment>
   )
 }
 
