efc539646ad83e13b647fd8b76a400406c166ef0
Thomas Russell <t.p.russell19@gmail.com>
2021-07-23 15:45:57 -0700

    :lipstick: :construction: WIP: attempting and failing at styling navbar

--- betsy-sauer/src/components/Navbar.js
+++ betsy-sauer/src/components/Navbar.js
@@ -1,22 +1,13 @@
 function Navbar(props) {
-  const navbarStyles = {
-    display: 'flex',
-    flexDirection: 'row',
-    background: 'black',
-    color: 'white',
-    alignContent: 'center',
-    alignItems: 'center',
-    overflow: 'none'
-  }
 
   return (
-    <div style={navbarStyles}>
-      <a type='submit' onClick={event => props.navigateTo(event, 'home')} href='/'>HOME</a>
-      <a type='submit' onClick={event => props.navigateTo(event, 'about')} href='/about'>ABOUT ME</a>
-      <a type='submit' onClick={event => props.navigateTo(event, 'gallery')} href='/gallery'>GALLERY</a>
-      <a type='submit' onClick={event => props.navigateTo(event, 'products')} href='/products'>PRODUCTS</a>
-      <a type='submit' onClick={event => props.navigateTo(event, 'appointment')} href='/appointment'>BOOK AN APPOINTMENT</a>
-    </div>
+    <ul className='navbar'>
+      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'home')} href='/'>HOME</a></li>
+      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'about')} href='/about'>ABOUT ME</a></li>
+      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'gallery')} href='/gallery'>GALLERY</a></li>
+      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'products')} href='/products'>PRODUCTS</a></li>
+      <li><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'appointment')} href='/appointment'>BOOK AN APPOINTMENT</a></li>
+    </ul>
   )
 }
 
