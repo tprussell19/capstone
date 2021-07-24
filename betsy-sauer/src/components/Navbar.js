7cc49500e907d611d8f81a42a387dc5611fb19cf
Thomas Russell <t.p.russell19@gmail.com>
2021-07-23 17:53:09 -0700

    :construction: WIP: still trying to figure out conditional styling

--- betsy-sauer/src/components/Navbar.js
+++ betsy-sauer/src/components/Navbar.js
@@ -2,19 +2,22 @@ import React from "react";
 
 function Navbar(props) {
 
-  let pageIndicator = '';
+  const homeActive = {
+    className: (props.currentPage === 'home' ? 'navbar-links active' : 'navbar-links')
+  }
 
   return (
     <React.Fragment>
       <ul className='navbar'>
-        <li className={pageIndicator + ' navbar-items'} {...props.currentPage === 'home' ? pageIndicator='active' : null }><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'home')} href='/'>HOME</a></li>
-        <li className='navbar-items' {...props.currentPage}><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'about')} href='/about'>ABOUT ME</a></li>
-        <li className='navbar-items' {...props.currentPage}><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'gallery')} href='/gallery'>GALLERY</a></li>
-        <li className='navbar-items' {...props.currentPage}><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'products')} href='/products'>PRODUCTS</a></li>
-        <li className='navbar-items' {...props.currentPage}><a type='submit' className='navbar-links'onClick={event => props.navigateTo(event, 'appointment')} href='/appointment'>BOOK AN APPOINTMENT</a></li>
+        <li className='navbar-items' ><a type='submit' style={homeActive} onClick={event => props.navigateTo(event, 'home')} href='/'>HOME</a></li>
+        <li className='navbar-items' ><a type='submit' className='navbar-links' onClick={event => props.navigateTo(event, 'about')} href='/about'>ABOUT ME</a></li>
+        <li className='navbar-items' ><a type='submit' className='navbar-links' onClick={event => props.navigateTo(event, 'gallery')} href='/gallery'>GALLERY</a></li>
+        <li className='navbar-items' ><a type='submit' className='navbar-links' onClick={event => props.navigateTo(event, 'products')} href='/products'>PRODUCTS</a></li>
+        <li className='navbar-items' ><a type='submit' className='navbar-links' onClick={event => props.navigateTo(event, 'appointment')} href='/appointment'>BOOK AN APPOINTMENT</a></li>
       </ul>
     </React.Fragment>
   )
 }
+
 
 export default Navbar;
