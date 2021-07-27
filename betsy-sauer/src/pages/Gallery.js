d4991d54ffa0c96135ee17edb4afab0027582107
Thomas Russell <t.p.russell19@gmail.com>
2021-07-27 09:53:16 -0700

    :construction: WIP: working through setup and implementation of instagram api

--- betsy-sauer/src/pages/Gallery.js
+++ betsy-sauer/src/pages/Gallery.js
@@ -4,6 +4,7 @@ function Gallery() {
       <h2>This is the GALLERY page</h2>
       <div>researching instagram api to put here</div>
       <div>setting up a facebook developer account to use the insta api</div>
+      <div>trying to authenticate test user in order to test if api will work</div>
     </div>
   )
 }
