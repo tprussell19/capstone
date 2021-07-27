f9fbf68cbafdd3f931bf319e73872209d2556870
Thomas Russell <t.p.russell19@gmail.com>
2021-07-27 12:17:57 -0700

    :construction: WIP: finally got successful fetch in postman and will implement in gallery with hooks

--- betsy-sauer/src/pages/Gallery.js
+++ betsy-sauer/src/pages/Gallery.js
@@ -1,12 +1,16 @@
+import React, { useState } from 'react';
+
 function Gallery() {
+
+
+
   return(
     <div className='container'>
       <h2>This is the GALLERY page</h2>
-      <div>researching instagram api to put here</div>
-      <div>setting up a facebook developer account to use the insta api</div>
-      <div>trying to authenticate test user in order to test if api will work</div>
     </div>
   )
 }
 
 export default Gallery;
+
+// successfully retrieved data from my instagram to test api in postman. Going to implement here using hooks
