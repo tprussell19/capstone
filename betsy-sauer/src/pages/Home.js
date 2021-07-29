123a603ad02bbec1a068456444a6465b56e55a32
Thomas Russell <t.p.russell19@gmail.com>
2021-07-29 10:34:33 -0700

    :lipstick: :hammer: add styling to home contact info

--- betsy-sauer/src/pages/Home.js
+++ betsy-sauer/src/pages/Home.js
@@ -4,16 +4,18 @@ function Home() {
   return(
     <div className='container home'>
       <div>
-        <h2>I AM OPEN AND TAKING NEW CLIENTS</h2>
+        <h4>I AM OPEN AND TAKING NEW CLIENTS</h4>
       </div>
       <div className='conatiner contact'>
         <div className='container map'>
           <img id='map' src={map} alt='Map of location of Amaryllis Salon in NW Portland' />
         </div>
         <div className='container contact-info'>
+          <hr />
           <ul id='salon'>
-            <li><strong>Amaryllis salon</strong></li>
-            <li>2368 NW Thurman St<br />Portland, OR 97210</li>
+            <strong>Amaryllis salon</strong>
+            <hr />
+            <li>2368 NW Thurman St<br />  Portland, OR 97210</li>
             <li>(503) 930-5231</li>
             <li>
               Monday	Closed<br />
@@ -25,8 +27,10 @@ function Home() {
               Sunday	Closed<br />
             </li>
           </ul>
+          <hr />
           <ul id='betsy-contact-info'>
-            <li><strong>Betsy Sauer</strong></li>
+            <strong>Betsy Sauer</strong>
+            <hr />
             <li>(503) 715-7763</li>
             <li><a href='mailto:betsysauer49@gmail.com'>betsysauer49@gmail.com</a></li>
           </ul>
